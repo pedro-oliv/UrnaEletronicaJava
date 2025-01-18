b6f9461e5b0c237b0146003669ca94101682ddb0
Pedro Oliveira <limapedro025@gmail.com>
2025-01-18 19:56:35 -0300

    update

--- UrnaEletronicaJava.java
+++ UrnaEletronicaJava.java
@@ -1,119 +1,49 @@
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-
+import java.util.List;
 
 public class UrnaEletronicaJava {
-    private int _votosNulos;
-    private List<Candidato> _candidatos;
-
-    public UrnaEletronicaJava(List<Candidato> candidatos, int votosNulos){
-        this._candidatos = candidatos;
-        this._votosNulos = votosNulos;
-    }
-
-    public boolean confirmarVoto(String mensagemConfirmacao){
-        @SuppressWarnings("resource") //gambiarra, provavelmente tem uma maneira melhor de fazer mas vai ficar assim mesmo, tá funcionando
+    static Processos processo;
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(true){
-            System.out.println(mensagemConfirmacao + " (S para sim, N para não)");
-            char resposta = sc.next().toUpperCase().charAt(0);
-            if(resposta == 'S'){
-                return true;
-            }
-            else if(resposta == 'N'){
-                return false;
-            }
-            else{
-                System.out.println("Entrada inválida. Digite 'S' para sim ou 'N' para não.");
+        startData();
+    
+        System.out.println("Bem-vindo à Urna Eletrônica!");
+        System.out.println("Candidatos:");
+        System.out.println("01 - Ada Lovelace");
+        System.out.println("02 - Alan Turing");
+        System.out.println("03 - Marie Curie");
+        System.out.println("04 - Albert Eistein");
+        System.out.println("05 - Ludwig van Beethoven");
+
+        int votosRecebidos=0;
+        while (votosRecebidos<10){
+            System.out.println("Digite o número do seu candidato (No formato certo: 01, 02, etc.):");
+            String numerostring = sc.nextLine();
+
+            boolean entradaAceita = processo.receberVoto(numerostring);
+
+            if(entradaAceita){
+                votosRecebidos++;
             }
         }
+        processo.exibirResultados();
+        sc.close();
     }
 
-    public boolean receberVoto(String numeroVoto){
-
-        if(numeroVoto.matches("\\d{2}")){
-            int numero = Integer.parseInt(numeroVoto);
-            boolean votoValido = false;
-
-        for(Candidato candidato : _candidatos){
-            if(candidato.getNumero() == numero){
-                String mensagem = "Seu voto vai para: " + candidato.getNome() + ", confirmar?";
-                if(confirmarVoto(mensagem)){
-                    candidato.incrementarVotos();
-                    votoValido = true;
-                }
-                else{
-                    System.out.println("Voto cancelado. Digite novamente");
-                }
-                return votoValido;
-
-            }
-        }
-        String mensagem = "Número inválido, seu voto será nulo, confirmar?";
-        if(confirmarVoto(mensagem)){
-            System.out.println("Voto nulo registrado.");
-            _votosNulos++;
-            return true;
-        }
-        else{
-            System.out.println("Voto cancelado. Digite novamente.");
-            return false;
-        }
-
+        public static void startData(){
+        List<Candidato> candidatos = new ArrayList<>();
+        Candidato candidato1 = new Candidato(1,"Ada Lovelace",0);
+        Candidato candidato2 = new Candidato(2, "Alan Turing", 0);
+        Candidato candidato3 = new Candidato(3, "Marie Curie", 0);
+        Candidato candidato4 = new Candidato(4, "Albert Einstein", 0);
+        Candidato candidato5 = new Candidato(5, "Ludwig van Beethoven", 0);
+        candidatos.add(candidato1);
+        candidatos.add(candidato2);
+        candidatos.add(candidato3);
+        candidatos.add(candidato4);
+        candidatos.add(candidato5);
+        processo = new Processos(candidatos,0);
     }
-        else if(numeroVoto.matches("\\d{3,}")){
-            System.out.println("Entrada recusada. Por favor digite um número válido no formato 01, 02, etc.");
-            return false;
-        }
-        else{
-            System.out.println("Entrada recusada. Por favor digite um número válido no formato 01, 02, etc.");
-            return false;
-        }
-        
-
-    }
-
-
-    public void apurarResultados(){
-        int totalVotosValidos =0;
-        for(Candidato candidato : _candidatos){
-            totalVotosValidos += candidato.getVotos();
-        }
-
-        double maxVotos=0;
-        List<Candidato> vencedores = new ArrayList<>();
-
-        for(Candidato candidato : _candidatos){
-            double percentual = totalVotosValidos > 0 ? (candidato.getVotos() * 100) / totalVotosValidos:0;
-            System.out.printf("%s: %d votos (%.1f%%)%n", candidato.getNome(), candidato.getVotos(), percentual);
-
-            if(candidato.getVotos()>maxVotos){
-                maxVotos = candidato.getVotos();
-                vencedores.clear();
-                vencedores.add(candidato);
-            }
-            else if(candidato.getVotos() == maxVotos){
-                vencedores.add(candidato);
-            }
-        }
-        System.out.printf("Votos Nulos: %d%n", _votosNulos);
-
-        if(vencedores.size() == 1){
-            System.out.printf("Vencedor: %s%n", vencedores.get(0).getNome());
-        }
-        else{
-            System.out.println("Houve um empate entre os seguntes candidatos: ");
-            for(Candidato vencedor : vencedores){
-                System.out.printf("%s%n", vencedor.getNome());
-            }
-        }
-    }
-
-    public void exibirResultados(){
-        System.out.println("Resultado da Votação:");
-        apurarResultados();
-    }
-
 }
